a277705d14ceec91bc78f00ee607e1f420c9ebdf
angelaw1 <angelawu.123456789@gmail.com>
2019-11-08 13:44:43 -0800

    set up development environment

---
+++ src/containers/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/index.css';
+
+class Landing extends React.Component {
+    render() {
+        return (
+            <p>Papyri</p>
+        )
+    }
+}
+
+export default Landing;
