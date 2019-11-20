7a768a2e0271cf481f7ef4bd6e5700c7bb3315bb
Lotus of Mystery <lizhenghao99@gmail.com>
2019-11-19 20:20:08 -0800

    restore registration to working state

--- backend/papyri/endpoints.py
+++ backend/papyri/endpoints.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
-from .api import RegistrationAPI, LoginAPI, UserAPI
+from .account import RegistrationAPI, LoginAPI, UserAPI
 
 urlpatterns = [
-    path('register/', RegistrationAPI.as_view()),
-    path('login/', LoginAPI.as_view()),
+    path('user/register/', RegistrationAPI.as_view()),
+    path('user/login/', LoginAPI.as_view()),
     path('user/', UserAPI.as_view())
 ]
--- backend/papyri/models.py
+++ backend/papyri/models.py
@@ -6,69 +6,70 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class UserInfo(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    uid = models.CharField(validators=[RegexValidator(r'^[0-9]{9}')], max_length=10)
-
+    uid = models.CharField(validators=[RegexValidator(r'^[0-9]{9}')], max_length=10, blank=True)
+    
     is_student = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.uid
-
+        return self.owner.username
 
 class ProfilePic(models.Model):
-    owner = models.ForeignKey(UserInfo, on_delete=models.CASCADE, 
-        related_name='profile_pic')
+    owner = models.ForeignKey(UserInfo, on_delete=models.CASCADE, related_name='profile_pic')
     pic1 = models.ImageField(upload_to='profile_pics')
     pic2 = models.ImageField(upload_to='profile_pics', blank=True)
     pic3 = models.ImageField(upload_to='profile_pics', blank=True)
     pic4 = models.ImageField(upload_to='profile_pics', blank=True)
     pic5 = models.ImageField(upload_to='profile_pics', blank=True)
-
-
-class ClassInfo(models.Model):
-    name = models.CharField(max_length=200)
-    teacher_id = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
-
-
-class UserClassRelationship(models.Model):
-    class_id = models.ForeignKey(ClassInfo, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE) 
-
-
-class Lecture(models.Model):
-    name = models.CharField(max_length=200)
-    slide_link = models.URLField()
-    class_id = models.ForeignKey(ClassInfo, on_delete=models.CASCADE)
-    date = models.DateField(auto_now_add=True)
-    in_session = models.BooleanField(default=False)
-
-
-class LectureAttendance(models.Model):
-    class_id = models.ForeignKey(ClassInfo, on_delete=models.CASCADE)
-    lecture_id = models.ForeignKey(Lecture, on_delete=models.CASCADE)
-    student_id = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
-class Quiz(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=2000)
-    time_created = models.DateTimeField(auto_now_add=True)
-    teacher_id = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
-
-
-
-class QuizQuestion(models.Model):
-    quiz_id = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-    question_number = models.PositiveIntegerField()
-    question = models.CharField(max_length=200)
     
-    answer_0 = models.CharField(max_length=200)
-    answer_1 = models.CharField(max_length=200)
-    answer_2 = models.CharField(max_length=200)
-    answer_3 = models.CharField(max_length=200)
+    def __str__(self):
+        return self.owner.owner.username
 
-    correct_answer = models.PositiveIntegerField(validators=[MaxValueValidator(3, message="Only accepts values from 0 to 3")])
+
+# class ClassInfo(models.Model):
+#     name = models.CharField(max_length=200)
+#     teacher_id = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
+
+
+# class UserClassRelationship(models.Model):
+#     class_id = models.ForeignKey(ClassInfo, on_delete=models.CASCADE)
+#     user_id = models.ForeignKey(User, on_delete=models.CASCADE) 
+
+
+# class Lecture(models.Model):
+#     name = models.CharField(max_length=200)
+#     slide_link = models.URLField()
+#     class_id = models.ForeignKey(ClassInfo, on_delete=models.CASCADE)
+#     date = models.DateField(auto_now_add=True)
+#     in_session = models.BooleanField(default=False)
+
+
+# class LectureAttendance(models.Model):
+#     class_id = models.ForeignKey(ClassInfo, on_delete=models.CASCADE)
+#     lecture_id = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+#     student_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+# class Quiz(models.Model):
+#     name = models.CharField(max_length=200)
+#     description = models.CharField(max_length=2000)
+#     time_created = models.DateTimeField(auto_now_add=True)
+#     teacher_id = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
+
+
+
+# class QuizQuestion(models.Model):
+#     quiz_id = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+#     question_number = models.PositiveIntegerField()
+#     question = models.CharField(max_length=200)
+    
+#     answer_0 = models.CharField(max_length=200)
+#     answer_1 = models.CharField(max_length=200)
+#     answer_2 = models.CharField(max_length=200)
+#     answer_3 = models.CharField(max_length=200)
+
+#     correct_answer = models.PositiveIntegerField(validators=[MaxValueValidator(3, message="Only accepts values from 0 to 3")])
 
 
 
