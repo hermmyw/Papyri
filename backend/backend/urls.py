58e1a26f9b2ed57d57beefcaf497cb0d40357e6c
Lotus of Mystery <lizhenghao99@gmail.com>
2019-11-19 21:18:44 -0800

    integrated custom user infos into registration

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -22,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('papyri.endpoints')),
     path('api/auth/', include('knox.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
