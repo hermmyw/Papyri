587020b99c0aa2027a39661778293e13a63c354b
angelaw1 <angelawu.123456789@gmail.com>
2019-11-21 18:20:09 -0800

    added api calls for registering functionality

--- src/containers/register/RegisInstr.js
+++ src/containers/register/RegisInstr.js
@@ -4,19 +4,55 @@ import { Container, Row, Col, Button, Form, FormGroup, Label, Input } from 'reac
 import { IoIosArrowBack } from "react-icons/io";
 import './Register.css';
 
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
 class RegisInstr extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {submitClicked: false};
+        this.state = {
+            submitClicked: false,
+            firstname: '',
+            lastname: '',
+            email: '',
+            password: '',
+            uid: ''
+        };
         this.handleSubmitClick = this.handleSubmitClick.bind(this);
         this.handleBackClick = this.handleBackClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleSubmitClick() {
-        console.log("student submitted!");
-        this.setState(state => ({
+    handleSubmitClick(e) {
+        e.preventDefault();
+        console.log("instructor submitted!");
+        console.log(`Email: ${ this.state.email }`)
+        /*this.setState(state => ({
             submitClicked: true
-        }));
+        }));*/
+
+        // make api call
+        fetch("http://127.0.0.1:8000/api/user/register/", {
+            method: "POST",
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username:  this.state.email,
+                email: this.state.email,
+                password: this.state.password,
+                first_name: this.state.firstname,
+                last_name: this.state.lastname,
+                uid: this.state.uid,
+                is_student: false
+            }),
+        })
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                }
+            )
     }
 
     handleBackClick() {
@@ -27,60 +63,112 @@ class RegisInstr extends React.Component {
         }));
         this.props.history.push('/');
     }
+    
+    handleChange = async (event) => {
+        const { target } = event;
+        const value = target.value;
+        const { name } = target;
+        await this.setState({
+            [ name ]: value,
+        });
+    }
 
     render() {
-        var display, backButton, submitButton = null;
+        const { firstname, lastname, uid, email, password } = this.state;
+        var display, backButton = null;
         if (!this.state.submitClicked) {
             display = (
                 <div>
-                    <Row><Col><p className="register-info">First name</p></Col></Row>
-                    <Row>
-                        <Col>
-                            <Form>
+                    <Form onSubmit={ (e) => this.handleSubmitClick(e) }>
+                        <Row>
+                            <Col>
                                 <FormGroup>
-                                    <Input className="register-input-center" type="name" name="firstname" id="exampleFirstName" />
+                                    <Label>First Name</Label>
+                                    <Input 
+                                        className="register-input-center" 
+                                        type="name" 
+                                        name="firstname" 
+                                        id="exampleFirstName" 
+                                        value={ firstname }
+                                        onChange={ (e) => {
+                                            this.handleChange(e)
+                                        }}
+                                    />
                                 </FormGroup>
-                            </Form>
-                        </Col>
-                    </Row>
-                    <Row><Col><p className="register-info">Last name</p></Col></Row>
-                    <Row>
-                        <Col>
-                            <Form>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
                                 <FormGroup>
-                                    <Input className="register-input-center" type="name" name="lastname" id="exampleLastName" />
+                                    <Label>Last Name</Label>
+                                    <Input 
+                                        className="register-input-center" 
+                                        type="name" 
+                                        name="lastname" 
+                                        id="exampleLastName" 
+                                        value={ lastname }
+                                        onChange={ (e) => {
+                                            this.handleChange(e)
+                                        }}
+                                    />
                                 </FormGroup>
-                            </Form>
-                        </Col>
-                    </Row>
-                    <Row><Col><p className="register-info">University ID</p></Col></Row>
-                    <Row>
-                        <Col>
-                        <Form>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
                             <FormGroup>
-                                <Input className="register-input-center" type="id" name="id" id="exampleId" />
+                                <Label>University ID</Label>
+                                <Input 
+                                    className="register-input-center" 
+                                    type="id" 
+                                    name="uid" 
+                                    id="exampleId" 
+                                    value={ uid }
+                                    onChange={ (e) => {
+                                        this.handleChange(e)
+                                    }}
+                                />
                             </FormGroup>
-                        </Form>
-                        </Col>
-                    </Row>
-                    <Row><Col><p className="register-info">Email</p></Col></Row>
-                    <Row>
-                    <Col>
-                        <Form>
-                            <FormGroup>
-                                <Input className="register-input-center" type="email" name="email" id="exampleEmail" />
-                            </FormGroup>
-                        </Form>
-                    </Col>
-                    </Row>
-                </div>
-            );
-
-            submitButton = (
-                <div>
-                    <Row>
-                        <Col><Button className="submit-button-center" block onClick={this.handleSubmitClick}>Submit</Button></Col>
-                    </Row>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <FormGroup>
+                                    <Label>Email</Label>
+                                    <Input 
+                                        className="register-input-center" 
+                                        type="email" 
+                                        name="email" 
+                                        id="exampleEmail"
+                                        value={ email }
+                                        onChange={ (e) => {
+                                            this.handleChange(e)
+                                        }} 
+                                    />
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <FormGroup>
+                                    <Label>Password</Label>
+                                    <Input 
+                                        className="register-input-center" 
+                                        type="password" 
+                                        name="password" 
+                                        id="examplePassword" 
+                                        value={password}
+                                        onChange={ (e) => {
+                                            this.handleChange(e)
+                                        }}
+                                    />
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col><Button type="submit" className="submit-button-center" block>Submit</Button></Col>
+                        </Row>
+                    </Form>
                 </div>
             );
 
@@ -118,7 +206,6 @@ class RegisInstr extends React.Component {
                 </Row>
                 <div>
                     {display}
-                    {submitButton}
                     {backButton}
                 </div>
             </Container>
