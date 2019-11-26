96efe8036803131b3970db039db59b044044c023
angelaw1 <angelawu.123456789@gmail.com>
2019-11-26 00:37:15 -0800

    created create class, create quiz, enroll in class
    also created specific class view for instructor
    updated routing

--- src/containers/CreateClass.js
+++ src/containers/CreateClass.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar.js';
-import { Container, Row, Col, Button, Form, FormGroup, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Input, Label, CustomInput} from 'reactstrap';
 import '../components/UI/UI.css';
+
 
 /**
  * Container for the Create Class page on the Instructor interface.
@@ -11,7 +12,33 @@ class CreateClass extends React.Component {
 
     constructor(props) {
         super(props);
+        var date = new Date,
+            years = [],
+            year = date.getFullYear();
+
+        for (var i = year; i < year + 5; i++) {
+            years.push(i);   
+        }
+
+        this.state = {
+            name: "",
+            term: "",
+            year: "",
+            yearsOptions: years,
+            days: {
+                monday: false,
+                tuesday: false,
+                wednesday: false,
+                thursday: false,
+                friday: false,
+                saturday: false,
+                sunday: false
+            }
+        }
+
         this.createClass = this.createClass.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+            
     }
 
     /**
@@ -19,22 +46,90 @@ class CreateClass extends React.Component {
      */
     createClass() {
 
+        // response to http request should contain access token
+        // go to new class page
+    }
+
+    handleSelectTerm(e) {
+        console.log("select term");
+
+        let options = e.target.options;
+        let termString = options[options.selectedIndex].value;
+        this.setState({term: termString});
+    }
+
+    handleSelectYear(e) {
+        console.log("select year");
+        let options = e.target.options;
+        let newYear = options[options.selectedIndex].value;
+        this.setState({year: newYear});
+    }
+
+    handleNameChange(e) {
+        console.log("name change");
+        console.log(e.target.value);
+        console.log(this);
+        this.setState({name: e.target.value});
+    }
+
+    handleDayChange(e) {
+        console.log("change day");
+        let day = e.target.id;
+        let checked = e.target.checked;
+        console.log("day: " + day);
+        console.log("checked: " + checked);
+        // this.setState({year: newYear});
+
+        var newDays = this.state.days;
+        newDays[day] = checked;
+        this.setState({days: newDays});
     }
 
     render() {
+        console.log("rendering");
+        console.log(this.state);
         return (
             <div>
                 <Sidebar view="create class" />
-                <div class="main-area">
-                    <Form  onSubmit={ e => this.createClass(e) }>
+                <div className="main-area">
+                    <Form  onSubmit={this.createClass}>
                         <FormGroup>
-                            <Input className="custom-input" type="email" name="email" id="className" placeholder="Class Name" />
+                            <Input 
+                                className="custom-input" 
+                                type="text" 
+                                name="class" 
+                                id="classID" 
+                                onChange={this.handleNameChange}
+                                value={this.state.name} 
+                                placeholder="Class ID" />
                         </FormGroup>
                         <FormGroup>
-                            <Input className="custom-input" type="password" name="password" id="classYear" placeholder="Year" />
+                            <Input type="select" bsSize="lg" name="term" onChange={ (e) => this.handleSelectTerm(e) }>
+                                <option selected disabled hidden>Term</option>
+                                <option value="fall">Fall</option>
+                                <option value="winter">Winter</option>
+                                <option value="spring">Spring</option>
+                            </Input>                        
                         </FormGroup>
                         <FormGroup>
-                            <Input className="custom-input" type="password" name="password" id="classID" placeholder="ID" />
+                            <Input type="select" bsSize="lg" name="year" onChange={ (e) => this.handleSelectYear(e) }>
+                                <option selected disabled hidden>Year</option>
+                                {this.state.yearsOptions.map((year) => 
+                                    <option value={year}>{year}</option>
+                                )}
+                            </Input>                        
+                        </FormGroup>
+                        <FormGroup>
+                            <Label>Lecture Days</Label>
+                            <div>
+                                <CustomInput type="checkbox" id="monday" label="Monday" onClick={ (e) => this.handleDayChange(e) }/>
+                                <CustomInput type="checkbox" id="tuesday" label="Tuesday"  onChange={ (e) => this.handleDayChange(e) }/>
+                                <CustomInput type="checkbox" id="wednesday" label="Wednesday"  onChange={ (e) => this.handleDayChange(e) }/>
+                                <CustomInput type="checkbox" id="thursday" label="Thursday"  onChange={ (e) => this.handleDayChange(e) }/>
+                                <CustomInput type="checkbox" id="friday" label="Friday"  onChange={ (e) => this.handleDayChange(e) }/>
+                                <CustomInput type="checkbox" id="saturday" label="Saturday"  onChange={ (e) => this.handleDayChange(e) }/>
+                                <CustomInput type="checkbox" id="sunday" label="Sunday"  onChange={ (e) => this.handleDayChange(e) }/>
+                            </div>
                         </FormGroup>
                         <FormGroup>
                             <Button className="yellow-button" size="lg" block type="submit">Create New Class</Button>
--- src/containers/CreateQuiz.js
+++ src/containers/CreateQuiz.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar.js';
+import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
 
 /**
  * Container for the Create Quiz page on the Instructor interface 
@@ -14,23 +15,118 @@ class CreateQuiz extends React.Component {
      */
     constructor(props) {
         super(props);
-        this.createQuiz = this.createQuiz.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        //this.handleAddAnswer = this.handleAddAnswer.bind(this);
+        this.state = {
+            nQuestions: 1
+        }
     }
 
     /**
      * Makes an http request to an endpoint to create a quiz.
      */
-    createQuiz() {
+    handleSubmit() {
 
+
+        // go back to quiz page after submit
     }
 
     render() {
+
         return (
             <div>
                 <Sidebar view="create quiz" />
                 <div className="main-area">
-                    <p>Create Quiz</p>
+                <div>
+                    <Form  onSubmit={ e => this.handleSubmit(e) }>
+                        <FormGroup>
+                            <Label>Quiz Question</Label>
+                            <Input className="custom-input" type="textarea" name="quizQuestion" id="quizQuestion" placeholder="Quiz Question" />
+                        </FormGroup>
+                        <AnswerList />
+                        <FormGroup>
+                            <Button className="yellow-button" size="lg" block onClick={this.handleSubmit}>Add Quiz</Button>
+                        </FormGroup>
+                    </Form>
                 </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class AnswerList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            answers: [{ answer: "" }]
+        };
+
+        console.log("constructor: ")
+        console.log(this.state.answers);
+    }
+
+    handleUpdateValue = (id, e) => {
+        console.log("update value: ");
+        const newAnswers = this.state.answers.map((currAnswer, index) => {
+            if (id !== index) return currAnswer;
+            return {...currAnswer, answer: e.target.value };
+        });
+        this.setState({ answers: newAnswers })
+    }
+
+    handleAddAnswer = () => {
+        console.log("add answer pressed");
+        this.setState({
+            answers: this.state.answers.concat([{answer: ""}])
+        });
+        console.log(this.state.answers);
+    }
+
+    handleDeleteAnswer = id => {
+        console.log("delete answer: " + id);
+        const newAnswers = this.state.answers.filter((currAnswer, index) => id !== index);
+        this.setState({ answers: newAnswers });
+    }
+
+    render() {
+        console.log("rendering: ")
+        console.log(this.state.answers);
+        var button = null;
+        if (this.state.answers.length < 5) {
+            button = (
+                <FormGroup>
+                    <Button 
+                        className="yellow-button" 
+                        size="lg" 
+                        block 
+                        onClick={this.handleAddAnswer}>Add Answer</Button>
+                </FormGroup>
+            )
+        }
+
+        return (
+            <div>
+                {this.state.answers.map((currAnswer, id) => 
+                    <FormGroup key={id}>
+                        <Input 
+                            className="custom-input" 
+                            type="text"
+                            id={id} 
+                            placeholder="Quiz Answer" 
+                            onChange={this.handleUpdateValue.bind(undefined, id)}
+                            value={currAnswer.answer}
+                        />
+                        <Button 
+                            className="yellow-button" 
+                            size="lg" 
+                            block 
+                            onClick={() => this.handleDeleteAnswer(id)}
+                        >
+                                delete answer</Button>
+                    </FormGroup>
+                )}
+                {button}
             </div>
         )
     }
