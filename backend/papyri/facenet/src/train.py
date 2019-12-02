7b389884dd8b77308d4a3f4849e823218e7dca94
lewishong <smlewishong@gmail.com>
2019-12-01 22:25:39 -0800

    Remove unnecessary model files

---
+++ backend/papyri/facenet/src/train.py
@@ -0,0 +1,129 @@
+"""An example of how to use your own dataset to train a classifier that recognizes people.
+"""
+# MIT License
+#
+# Copyright (c) 2016 David Sandberg
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+import argparse
+import facenet
+import os
+import sys
+import math
+import pickle
+from sklearn.svm import SVC
+
+# def main(args):
+def train(data_dir, model, classifier_filename):
+
+    image_size = 160
+    rand_seed = 666
+    batch_size = 1000
+
+    with tf.Graph().as_default():
+
+        with tf.Session() as sess:
+
+            np.random.seed(seed=rand_seed)
+
+            # if args.use_split_dataset:
+            #     dataset_tmp = facenet.get_dataset(args.data_dir)
+            #     train_set, test_set = split_dataset(dataset_tmp, args.min_nrof_images_per_class, args.nrof_train_images_per_class)
+            #     dataset = train_set
+            # else:
+            dataset = facenet.get_dataset(data_dir)
+
+            # Check that there are at least one training image per class
+            for cls in dataset:
+                assert(len(cls.image_paths)>0, 'There must be at least one image for each class in the dataset')
+
+
+            paths, labels = facenet.get_image_paths_and_labels(dataset)
+
+            print('Number of classes: %d' % len(dataset))
+            print('Number of images: %d' % len(paths))
+
+            # Load the model
+            print('Loading feature extraction model')
+            facenet.load_model(model)
+
+            # Get input and output tensors
+            images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+            embedding_size = embeddings.get_shape()[1]
+
+            # Run forward pass to calculate embeddings
+            print('Calculating features for images')
+            nrof_images = len(paths)
+            nrof_batches_per_epoch = int(math.ceil(1.0*nrof_images / batch_size))
+            emb_array = np.zeros((nrof_images, embedding_size))
+            for i in range(nrof_batches_per_epoch):
+                start_index = i*batch_size
+                end_index = min((i+1)*batch_size, nrof_images)
+                paths_batch = paths[start_index:end_index]
+                images = facenet.load_data(paths_batch, False, False, image_size)
+                feed_dict = { images_placeholder:images, phase_train_placeholder:False }
+                emb_array[start_index:end_index,:] = sess.run(embeddings, feed_dict=feed_dict)
+
+            classifier_filename_exp = os.path.expanduser(classifier_filename)
+
+            # Train classifier
+            print('Training classifier')
+            model = SVC(kernel='linear', probability=True)
+            model.fit(emb_array, labels)
+
+            # Create a list of class names
+            class_names = [ cls.name.replace('_', ' ') for cls in dataset]
+
+            # Saving classifier model
+            with open(classifier_filename_exp, 'wb') as outfile:
+                pickle.dump((model, class_names), outfile)
+            print('Saved classifier model to file "%s"' % classifier_filename_exp)
+
+
+def split_dataset(dataset, min_nrof_images_per_class, nrof_train_images_per_class):
+    train_set = []
+    test_set = []
+    for cls in dataset:
+        paths = cls.image_paths
+        # Remove classes with less than min_nrof_images_per_class
+        if len(paths)>=min_nrof_images_per_class:
+            np.random.shuffle(paths)
+            train_set.append(facenet.ImageClass(cls.name, paths[:nrof_train_images_per_class]))
+            test_set.append(facenet.ImageClass(cls.name, paths[nrof_train_images_per_class:]))
+    return train_set, test_set
+
+
+def main():
+    data_dir = 'datasets/input_mtcnnpy_160'
+    model = 'models/20180402-114759/20180402-114759.pb'
+    classifier_filename = 'models/my_classifier.pkl'
+
+    prediction = train(data_dir, model, classifier_filename)
+
+if __name__ == '__main__':
+    main()
