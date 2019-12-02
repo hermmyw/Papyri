271a6adf81ec0c153fa40f138d8fe9078bc43b39
Lotus of Mystery <lizhenghao99@gmail.com>
2019-12-01 19:41:31 -0800

    added doc for account and quiz apis

--- backend/papyri/quiz.py
+++ backend/papyri/quiz.py
@@ -4,10 +4,27 @@ from rest_framework.response import Response
 from .serializers import QuizSerializer, AnswerSerializer, ResultSerializer
 from .models import Quiz, Answer, Result
 
+'''
+@api {post} /api/quiz/create/ Create quiz
+@apiName CreateQuizAPI
+
+@apiParam {String}  name        Quiz title.
+@apiParam {String}  question    Quiz question description.
+@apiParam {String}  class_id    Class id of the quiz.
+@apiParam {String}  answer_0    Description of first choice.
+@apiParam {String}  answer_1    Description of second choice.
+@apiParam {String}  answer_2    Description of third choice.
+@apiParam {String}  answer_3    Description of fourth choice.
+@apiParam {Number}  correct_answer  Correct choice.
+'''
 class CreateQuizAPI(generics.CreateAPIView):
     serializer_class = QuizSerializer
     queryset = Quiz.objects.all()
 
+'''
+@api {put} /api/quiz/activate/<int:id>/ Activate/open quiz
+@apiName ActivateQuizAPI
+'''
 class ActivateQuizAPI(generics.RetrieveUpdateAPIView):
     serializer_class = QuizSerializer
     queryset = Quiz.objects.all()
@@ -19,6 +36,11 @@ class ActivateQuizAPI(generics.RetrieveUpdateAPIView):
         instance.save()
         return Response(QuizSerializer(instance).data)
 
+
+'''
+@api {put} /api/quiz/release/<int:id>/ Release/close quiz and generate result
+@apiName ReleaseQuizAPI
+'''
 class ReleaseQuizAPI(generics.RetrieveUpdateAPIView):
     serializer_class = QuizSerializer
     queryset = Quiz.objects.all()
@@ -60,6 +82,23 @@ class ReleaseQuizAPI(generics.RetrieveUpdateAPIView):
 
         return Response(QuizSerializer(instance).data)
 
+'''
+@api {get} /api/quiz/list/[unreleased,released,active]/<int:class_id> List unreleased/released/active quizzes for a class
+@apiName ListQuizAPI
+
+@apiSuccess {Object[]} quizzes  List of quizzes
+@apiSuccess {String}  quizzes.name        Quiz title.
+@apiSuccess {String}  quizzes.question    Quiz question description.
+@apiSuccess {String}  quizzes.class_id    Class id of the quiz.
+@apiSuccess {String}  quizzes.answer_0    Description of first choice.
+@apiSuccess {String}  quizzes.answer_1    Description of second choice.
+@apiSuccess {String}  quizzes.answer_2    Description of third choice.
+@apiSuccess {String}  quizzes.answer_3    Description of fourth choice.
+@apiSuccess {Number}  quizzes.correct_answer  Correct choice.
+@apiSuccess {String}  quizzes.time_created  Time of quiz creation.
+@apiSuccess {Boolean} quizzes.active      Flag of active quiz.
+@apiSuccess {Boolean} quizzes.released    Flag of released quiz.
+'''
 class ListQuizAPI(generics.ListAPIView):
     serializer_class = QuizSerializer
     lookup_field = 'class_id'
@@ -80,15 +119,36 @@ class ListQuizAPI(generics.ListAPIView):
             queryset = queryset.filter(active=True)
         return queryset
 
+'''
+@api {delete} /api/quiz/destroy/<int:id>/ Destroy a quiz
+@apiName DestroyQuizAPI
+'''
 class DestroyQuizAPI(generics.DestroyAPIView):
     serializer_class = QuizSerializer
     queryset = Quiz.objects.all()
     lookup_field = 'id'
 
+'''
+@api {post} /api/answer/create/ Create an answer submission
+@apiName CreateAnswerAPI
+
+@apiParam {Number}  choice      Student's choice to question.
+@apiParam {String}  quiz_id     Reference to the quiz.
+@apiParam {String}  student     Reference to the student of submission.
+'''
 class CreateAnswerAPI(generics.CreateAPIView):
     serializer_class = AnswerSerializer
     queryset = Answer.objects.all()
 
+'''
+@api {get} /api/answer/quiz/<quiz_id>/ List answer submissions for a quiz
+@apiName AnswerByQuizAPI
+
+@apiSuccess {Object[]} answers    List of student answer submissions.
+@apiSuccess {Number}  answers.choice      Student's choice to question.
+@apiSuccess {String}  answers.quiz_id     Reference to the quiz.
+@apiSuccess {String}  answers.student     Reference to the student of submission.
+'''
 class AnswerByQuizAPI(generics.ListAPIView):
     serializer_class = AnswerSerializer
     queryset = Answer.objects.all()
@@ -99,6 +159,15 @@ class AnswerByQuizAPI(generics.ListAPIView):
         queryset = Answer.objects.filter(quiz_id=quiz_id)
         return queryset
 
+'''
+@api {get} /api/answer/student/<int:student>/ List answer submissions of a student
+@apiName AnswerByStudentAPI
+
+@apiSuccess {Object[]} answers    List of student answer submissions.
+@apiSuccess {Number}  answers.choice      Student's choice to question.
+@apiSuccess {String}  answers.quiz_id     Reference to the quiz.
+@apiSuccess {String}  answers.student     Reference to the student of submission.
+'''
 class AnswerByStudentAPI(generics.ListAPIView):
     serializer_class = AnswerSerializer
     queryset = Answer.objects.all()
@@ -109,6 +178,18 @@ class AnswerByStudentAPI(generics.ListAPIView):
         queryset = Answer.objects.filter(student=student)
         return queryset
 
+'''
+@api {get} /api/quiz/result/<int:quiz_id>/ Get the result for a quiz
+@apiName RetrieveResultAPI
+
+@apiSuccess {String}    quiz_id         Refernce to the quiz.
+@apiSuccess {Number}    correct_answer  Correct choice of the quiz.
+@apiSuccess {Number}    num_students    Number of student submissions.
+@apiSuccess {Number}    choice_0_percent    Percentage of students that choose first choice.
+@apiSuccess {Number}    choice_1_percent    Percentage of students that choose second choice.
+@apiSuccess {Number}    choice_2_percent    Percentage of students that choose third choice.
+@apiSuccess {Number}    choice_3_percent    Percentage of students that choose fourth choice.
+'''
 class RetrieveResultAPI(generics.RetrieveAPIView):
     serializer_class = ResultSerializer
     queryset = Result.objects.all()
