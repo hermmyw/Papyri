fd27adeaee83cf4028672f0395f422f6e67b1b47
Lotus of Mystery <lizhenghao99@gmail.com>
2019-12-01 18:52:21 -0800

    stores profile pics by username

--- backend/papyri/models.py
+++ backend/papyri/models.py
@@ -15,13 +15,16 @@ class UserInfo(models.Model):
     def __str__(self):
         return self.owner.username
 
+def upload_path_handler(instance, filename):
+    return 'profile_pics/{uid}/{filename}'.format(uid=instance.owner, filename=filename)
+
 class ProfilePic(models.Model):
     owner = models.ForeignKey(UserInfo, on_delete=models.CASCADE, related_name='profile_pic')
-    pic1 = models.ImageField(upload_to='profile_pics', blank=True)
-    pic2 = models.ImageField(upload_to='profile_pics', blank=True)
-    pic3 = models.ImageField(upload_to='profile_pics', blank=True)
-    pic4 = models.ImageField(upload_to='profile_pics', blank=True)
-    pic5 = models.ImageField(upload_to='profile_pics', blank=True)
+    pic1 = models.ImageField(upload_to=upload_path_handler, blank=True)
+    pic2 = models.ImageField(upload_to=upload_path_handler, blank=True)
+    pic3 = models.ImageField(upload_to=upload_path_handler, blank=True)
+    pic4 = models.ImageField(upload_to=upload_path_handler, blank=True)
+    pic5 = models.ImageField(upload_to=upload_path_handler, blank=True)
     
     def __str__(self):
         return self.owner.owner.username
